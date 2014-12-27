efa81d3e339691a0172d7061d7f23370ae8760c8
I <antngribv@yandex.ru>
2014-12-27 12:56:12 +0200

    Initial commit

---
+++ DiplomaProject/DiplomaProject/Controls/ResizableContainer/ResizableContainer.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace DiplomaProject.Controls.ResizableContainer {
+    public class ResizableContainer : ContentControl
+    {
+        private ResizeGrip partResizeGrip;
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            partResizeGrip = GetTemplateChild("PartResizeGrip") as ResizeGrip;
+            partResizeGrip.MouseEnter += partResizeGrip_MouseEnter;
+            partResizeGrip.MouseLeave += partResizeGrip_MouseLeave;
+        }
+
+        void partResizeGrip_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+        void partResizeGrip_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e) {
+            Mouse.OverrideCursor = Cursors.SizeNWSE;
+        }
+    }
+}
