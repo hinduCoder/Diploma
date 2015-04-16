e4e57244aea9a23dcfe9af939819480a19cbc13c
Anton <antngribv@yandex.ru>
2015-04-16 13:14:30 +0300

    Smooth drawer

--- DiplomaProject/DiplomaProject/Controls/Drawer/DrawerControl.cs
+++ DiplomaProject/DiplomaProject/Controls/Drawer/DrawerControl.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Ink;
 using System.Windows.Input;
-using DevExpress.Mvvm;
-using Xceed.Wpf.Toolkit;
+using System.Windows.Media;
+using DiplomaProject.Annotations;
+using MathNet.Numerics.Interpolation;
 
 namespace DiplomaProject.Controls
 {
@@ -44,6 +45,35 @@ namespace DiplomaProject.Controls
         public StrokeCollection Strokes {
             get { return (StrokeCollection)GetValue(StrokesProperty); }
             set { SetValue(StrokesProperty, value); }
+        }
+    }
+
+    public class SmoothableStroke : Stroke {
+        public SmoothableStroke([NotNull] StylusPointCollection stylusPoints) : base(stylusPoints) { }
+        public SmoothableStroke([NotNull] StylusPointCollection stylusPoints, [NotNull] DrawingAttributes drawingAttributes) : base(stylusPoints, drawingAttributes) { }
+        protected override void DrawCore(DrawingContext context, DrawingAttributes overrides) {
+            var points = DrawingAttributes.FitToCurve ? GetBezierStylusPoints() : StylusPoints;
+            var reducedPoints = GeometryHelper.DouglasPeuckerReduction(points.Select(sp => sp.ToPoint()).ToList(), 10d);
+            var spline = CubicSpline.InterpolateNatural(reducedPoints.Select(p => p.X), reducedPoints.Select(p => p.Y));
+            Point? prevPoint = null;
+            for(var i = points.Min(sp => sp.X); i <= points.Max(sp => sp.X); i++) {
+                var curPoint = new Point(i, spline.Interpolate(i));
+                if(prevPoint != null)
+                    context.DrawLine(new Pen(Brushes.Black, 2), prevPoint.Value, curPoint);
+                prevPoint = curPoint;
+            }
+        }
+    }
+
+    public class InkCanvasEx : InkCanvas {
+        protected override void OnStrokeCollected(InkCanvasStrokeCollectedEventArgs e) {
+            Strokes.Remove(e.Stroke);
+            var customStroke = new SmoothableStroke(e.Stroke.GetBezierStylusPoints());
+            Strokes.Add(customStroke);
+
+            InkCanvasStrokeCollectedEventArgs args =
+                new InkCanvasStrokeCollectedEventArgs(customStroke);
+            base.OnStrokeCollected(args);
         }
     }
 }
