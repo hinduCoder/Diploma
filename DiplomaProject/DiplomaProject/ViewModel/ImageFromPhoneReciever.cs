cf63354af7510ed233a47173ae990a88001ad365
Anton <antngribv@yandex.ru>
2015-04-18 23:54:23 +0300

    Temp commit (fix android)

--- DiplomaProject/DiplomaProject/ViewModel/ImageFromPhoneReciever.cs
+++ DiplomaProject/DiplomaProject/ViewModel/ImageFromPhoneReciever.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
@@ -8,24 +10,31 @@ namespace DiplomaProject.ViewModel
 {
     public class ImageFromPhoneReciever
     {
-        public Task<BitmapSource> RecieveAsync()
+        public async Task<BitmapSource> RecieveAsync()
         {
-            return Task<BitmapSource>.Factory.StartNew(Recieve);
-        }
-
-        private static BitmapSource Recieve()
-        {
-            byte[] buffer;
-            using (var socket = new Socket(SocketType.Dgram, ProtocolType.Udp))
+            JpegBitmapDecoder jpegBitmapDecoder;
+            using (var stream = new MemoryStream())
             {
-                EndPoint endPoint = new IPEndPoint(IPAddress.Any, 50001);
-                socket.Bind(endPoint);
-                buffer = new byte[10000000];
-                socket.ReceiveFrom(buffer, ref endPoint);
+                using (var socket = new Socket(SocketType.Stream, ProtocolType.Tcp))
+                {
+                    EndPoint endPoint = new IPEndPoint(IPAddress.Any, 50001);
+                    socket.Bind(endPoint);
+                    socket.Listen(1);
+                    var acceptSocket = await Task.Factory.StartNew<Socket>(socket.Accept);
+                    var bufferSize = 10000;
+                    var buffer = new byte[bufferSize];
+                    int received;
+                    do
+                    {
+                        received = acceptSocket.Receive(buffer, bufferSize, SocketFlags.None);
+                        stream.Write(buffer, 0, received);
+                    } while (received > 0);
+                }
+                stream.Seek(0, SeekOrigin.Begin);
+                jpegBitmapDecoder = new JpegBitmapDecoder(stream, BitmapCreateOptions.None, BitmapCacheOption.None);
             }
-            var stream = new MemoryStream(buffer);
-            var jpegBitmapDecoder = new JpegBitmapDecoder(stream, BitmapCreateOptions.None, BitmapCacheOption.None);
             return jpegBitmapDecoder.Frames[0];
         }
+
     }
 }
