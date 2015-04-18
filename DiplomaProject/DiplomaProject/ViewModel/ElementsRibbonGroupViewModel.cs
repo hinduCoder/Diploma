8b2cbeeca6f72ae9017a61f17f2386ca4ad2cce9
Anton <antngribv@yandex.ru>
2015-04-18 15:32:04 +0300

    Add image button

--- DiplomaProject/DiplomaProject/ViewModel/ElementsRibbonGroupViewModel.cs
+++ DiplomaProject/DiplomaProject/ViewModel/ElementsRibbonGroupViewModel.cs
@@ -1,6 +1,10 @@
-﻿using System.Windows.Documents;
+﻿using System;
+using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using DevExpress.Mvvm;
+using Microsoft.Win32;
 
 namespace DiplomaProject.ViewModel
 {
@@ -25,6 +29,21 @@ namespace DiplomaProject.ViewModel
         public ICommand AddDrawingCommand
         {
             get { return new DelegateCommand(() => _formattingProvider.AddDrawing(_documentState.CurrentSelection)); }
+        }
+
+        public ICommand AddImageCommand
+        {
+            get { return new DelegateCommand(AddImage); }
+        }
+
+        private void AddImage()
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image files (*.jpg, *.png) | *.jpg;  *.png"
+            };
+            openFileDialog.ShowDialog();
+            _formattingProvider.AddImage(_documentState.CurrentSelection, new BitmapImage(new Uri(openFileDialog.FileName)));
         }
 
         public ICommand AddFromPhoneCommand
